d832abe95ecb468b65007630cf8adadafb9ad48c
abrahamalen <alenabraham4874@gmail.com>
2022-04-12 23:44:59 +0530

    changes made
    Signed-off-by: abrahamalen <alenabraham4874@gmail.com>

--- src/main/java/com/project/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/project/githubcicdactions/GithubCicdActionsApplication.java
@@ -2,6 +2,8 @@ package com.project.githubcicdactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
